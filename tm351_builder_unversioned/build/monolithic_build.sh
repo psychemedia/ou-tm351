a1cf6f6c4082ec64345e10b95d69510dbab29c22
Tony Hirst <tony.hirst@gmail.com>
2017-05-26 14:00:10 +0100

    Update builder

--- tm351_builder_unversioned/build/monolithic_build.sh
+++ tm351_builder_unversioned/build/monolithic_build.sh
@@ -7,26 +7,26 @@ $THISDIR/version.sh
 
 #Build script for building machine
 $THISDIR/base/basepackages.sh
-#$THISDIR/base/basepy.sh
+$THISDIR/base/basepy.sh
 
 
 #Jupyter space
-#$THISDIR/jupyter-base/build_jupyter.sh
-#$THISDIR/jupyter-custom/jupyter_nbextensions.sh
-#$THISDIR/jupyter-custom/jupyter_styling.sh
+$THISDIR/jupyter-base/build_jupyter.sh
+$THISDIR/jupyter-custom/jupyter_nbextensions.sh
+$THISDIR/jupyter-custom/jupyter_styling.sh
 
 #Local extensions currently a bit broken...
-#$THISDIR/jupyter-custom/jupyter_extensions.sh
+$THISDIR/jupyter-custom/jupyter_extensions.sh
 
-#$THISDIR/pystack/build_tm351_stack.sh
+$THISDIR/pystack/build_tm351_stack.sh
 
 $THISDIR/openrefine/openrefine.sh
 
-#$THISDIR/postgres/postgresql.sh
+$THISDIR/postgres/postgresql.sh
 
-#$THISDIR/mongo/mongo.sh
-#$THISDIR/mongo/simple/mongo_simple.sh
-#$THISDIR/mongo/sharded/mongo_cluster.sh
+$THISDIR/mongo/mongo.sh
+$THISDIR/mongo/simple/mongo_simple.sh
+$THISDIR/mongo/sharded/mongo_cluster.sh
 
 #tidy up
 apt-get autoremove -y && apt-get clean && updatedb
